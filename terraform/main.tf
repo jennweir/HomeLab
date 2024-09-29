7078de53fb0e2bda5d6363ac60e7d29cde319469
Jennifer Weir <jennweir@umich.edu>
2024-09-29 13:51:19 -0400

    feat(wif): figuring out service accounts

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,8 @@
 locals {
-    K8S_NAMESPACE = "kubernetes-dashboard"
-    K8S_SERVICE_ACCOUNT = "kubernetes-dashboard"
+    K8S_NAMESPACE_DASHBOARD = "kubernetes-dashboard"
+    K8S_SERVICE_ACCOUNT_DASHBOARD = "kubernetes-dashboard"
+    K8S_NAMESPACE_CERT_MAN = "cert-manager"
+    K8S_SERVICE_ACCOUNT_CERT_MAN = "cert-manager-sa"
     K8S_GCP_PROJECT_NUMBER = "494599251997"
     K8S_WORKLOAD_IDENTITY_POOL = "k8s-wif-pool"
 }
@@ -18,5 +20,13 @@ resource "google_service_account_iam_member" "kubernetes-dashboard" {
     service_account_id = google_service_account.gcp_wif_service_account.name
     role               = "roles/iam.workloadIdentityUser"
     # principal://iam.googleapis.com/projects/494599251997/locations/global/workloadIdentityPools/k8s-wif-pool/subject/SUBJECT_ATTRIBUTE_VALUE (subject of OIDC token)
-    member             = "principal://iam.googleapis.com/projects/${local.K8S_GCP_PROJECT_NUMBER}/locations/global/workloadIdentityPools/${local.K8S_WORKLOAD_IDENTITY_POOL}/subject/${local.K8S_NAMESPACE}/${local.K8S_SERVICE_ACCOUNT}"
+    member             = "principal://iam.googleapis.com/projects/${local.K8S_GCP_PROJECT_NUMBER}/locations/global/workloadIdentityPools/${local.K8S_WORKLOAD_IDENTITY_POOL}/subject/${local.K8S_NAMESPACE_DASHBOARD}/${local.K8S_SERVICE_ACCOUNT_DASHBOARD}"
+}
+
+# GCP policy WIF binding: what allows our k8s service account to impersonate GCP service account
+resource "google_service_account_iam_member" "cert-manager-sa" {
+    service_account_id = google_service_account.gcp_wif_service_account.name
+    role               = "roles/iam.workloadIdentityUser"
+    # principal://iam.googleapis.com/projects/494599251997/locations/global/workloadIdentityPools/k8s-wif-pool/subject/SUBJECT_ATTRIBUTE_VALUE (subject of OIDC token)
+    member             = "principal://iam.googleapis.com/projects/${local.K8S_GCP_PROJECT_NUMBER}/locations/global/workloadIdentityPools/${local.K8S_WORKLOAD_IDENTITY_POOL}/subject/${local.K8S_NAMESPACE_CERT_MAN}/${local.K8S_SERVICE_ACCOUNT_CERT_MAN}"
 }
