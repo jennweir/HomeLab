5a0ebcf1bd9ff97123338daf698bdfc053590f72
Jennifer Weir <91640709+jennweir@users.noreply.github.com>
2024-10-20 22:27:04 -0400

    feat(cert-manager): jenniferpweir.com certificate (#19)

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,4 @@
 provider "google" {
-  # include access_token to set up wif
-  # gcloud auth login
   # access_token = $(gcloud auth print-access-token) for temporary authentication
   project = "pi-cluster-433101"
   region  = "us-east1"
