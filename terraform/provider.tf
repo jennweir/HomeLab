8ff2cee1493295d627e123bd0830fb0103749717
Jennifer Weir <jennweir@umich.edu>
2024-11-23 20:02:46 -0500

    fix(provider): token

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,5 @@
 provider "google" {
   # access_token = $(gcloud auth print-access-token) for temporary authentication
-  access_token = "ya29.a0AeDClZADcjAyNoJCbrfNrgTcOpocXTnPnnlu0zg6MolBmB5J1uoQZZlR1eLREF4i9F7f-gH-6bkIyL9Uuk_6Fovg6wZ8Bq_PETObyAiCpzs8vBVv45kVK4RheDiLy-gFRVhO9F1HJaIYuOdLQ10IG-LiLIUwGCZvm9Val3wluzixNCUaCgYKAcQSARISFQHGX2MiLtOZjxqmb3YH9QFsChlo-Q0182"
   project = "pi-cluster-433101"
   region  = "us-east1"
 }
