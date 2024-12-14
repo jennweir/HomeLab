5ed75356ca9f4382c907c38355ac07ab0fd413f5
Jennifer Weir <91640709+jennweir@users.noreply.github.com>
2024-12-14 01:47:57 -0500

    fix: refactor some folders into base and overlays (#30)
    * feat(terraform): oidc
    
    * feat(terraform): oidc
    
    * fix(provider): token
    
    * feat(cert-manager): volume mount
    
    * feat(cert)
    
    * feat(cert-manager): patches for google creds
    
    * fix(terraform): pool name
    
    * fix(creds)
    
    * fix(terraform): bucket name
    
    * fix(api-server): service-account-jwks-uri flag
    
    * fix(api-server): service-account-jwks-uri flag
    
    * fix: refactor into base and overlays

--- kubernetes/argocd/README.md
+++ kubernetes/argocd/README.md
@@ -2,4 +2,4 @@
 
 Username: admin
 
-Password: `kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 --decode`
+Password: `kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 --decode | pbcopy`
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,16 @@
 locals {
+    K8S_WORKLOAD_IDENTITY_POOL = "pi-cluster"
     K8S_NAMESPACE_CERT_MAN = "cert-manager"
     K8S_SERVICE_ACCOUNT_CERT_MAN = "cert-manager"
-    K8S_GCP_PROJECT_NUMBER = "494599251997"
-    K8S_WORKLOAD_IDENTITY_POOL = "k8s-wif-pool"
 }
 
-# GCP service account that the kubernetes service account impersonates
-# can create it via Terraform pipeline
-resource "google_service_account_iam_member" "cert_manager_binding" {
-    service_account_id = "projects/pi-cluster-433101/serviceAccounts/dns01-solver@pi-cluster-433101.iam.gserviceaccount.com"
-    role               = "roles/iam.workloadIdentityUser"
-    member             = "principal://iam.googleapis.com/projects/${local.K8S_GCP_PROJECT_NUMBER}/locations/global/workloadIdentityPools/${local.K8S_WORKLOAD_IDENTITY_POOL}/subject/${local.K8S_NAMESPACE_CERT_MAN}/${local.K8S_SERVICE_ACCOUNT_CERT_MAN}"
-}
-
-data "google_project" "pi_homelab_bucket" {
+data "google_project" "pi_cluster" {
     project_id = "pi-cluster-433101"
 }
 
 resource "google_storage_bucket" "pi_homelab_bucket" {
-    project = data.google_project.pi_homelab_bucket.project_id
-    name = "pi-cluster-bucket"
+    project = data.google_project.pi_cluster.project_id
+    name = "pi-cluster"
     location = "US"
     force_destroy = true
     public_access_prevention = "inherited"
@@ -30,4 +21,46 @@ resource "google_storage_bucket_iam_member" "pi_homelab_bucket" {
     bucket = google_storage_bucket.pi_homelab_bucket.name
     role   = "roles/storage.objectViewer"
     member = "allUsers"
+}
+
+resource "google_iam_workload_identity_pool" "pi_cluster" {
+    project                   = data.google_project.pi_cluster.project_id
+    workload_identity_pool_id = "pi-cluster"
+    display_name              = "pi-cluster"
+    description               = "created with terraform"
+}
+
+resource "google_iam_workload_identity_pool_provider" "pi_cluster" {
+    project = data.google_project.pi_cluster.project_id
+    display_name                       = "pi-cluster"
+    description                        = "created with terraform"
+    workload_identity_pool_id          = google_iam_workload_identity_pool.pi_cluster.workload_identity_pool_id
+    workload_identity_pool_provider_id = "pi-cluster"
+    attribute_mapping = {
+        "google.subject" = "assertion.sub"
+    }
+    oidc {
+        issuer_uri        = "https://storage.googleapis.com/pi-cluster"
+        allowed_audiences = [
+        "//iam.googleapis.com/projects/${data.google_project.pi_cluster.number}/locations/global/workloadIdentityPools/pi-cluster/providers/pi-cluster",
+        ]
+    }
+}
+
+resource "google_service_account" "dns_solver" {
+    account_id   = "cert-manager-dns-solver"
+    display_name = "Cert Manager DNS Solver"
+}
+
+resource "google_project_iam_member" "dns_solver_dns_admin" {
+    project = data.google_project.pi_cluster.project_id
+    role    = "roles/dns.admin"
+    member  = "serviceAccount:${google_service_account.dns_solver.email}" # cert-manager-dns-solver@pi-cluster-433101.iam.gserviceaccount.com
+}
+
+# k8s service account cert-manager to google service account cert-manager-dns-solver
+resource "google_service_account_iam_member" "cert_manager_binding" {
+    service_account_id = google_service_account.dns_solver.id
+    role               = "roles/iam.workloadIdentityUser"
+    member             = "principal://iam.googleapis.com/projects/${data.google_project.pi_cluster.number}/locations/global/workloadIdentityPools/${local.K8S_WORKLOAD_IDENTITY_POOL}/subject/${local.K8S_NAMESPACE_CERT_MAN}/${local.K8S_SERVICE_ACCOUNT_CERT_MAN}"
 }
