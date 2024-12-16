35e6d4223ef8c4fd027d80e8820d29e35ec6f5ce
Jennifer Weir <jennweir@umich.edu>
2024-12-15 22:29:26 -0500

    fix(cert-manager): try renaming sa email but still facing GoogleCloud API call failed: googleapi: Error 403: Forbidden, forbidden error

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,4 @@
 locals {
-    K8S_WORKLOAD_IDENTITY_POOL = "pi-cluster"
     K8S_NAMESPACE_CERT_MAN = "cert-manager"
     K8S_SERVICE_ACCOUNT_CERT_MAN = "cert-manager"
 }
@@ -42,25 +41,25 @@ resource "google_iam_workload_identity_pool_provider" "pi_cluster" {
     oidc {
         issuer_uri        = "https://storage.googleapis.com/pi-cluster"
         allowed_audiences = [
-        "//iam.googleapis.com/projects/${data.google_project.pi_cluster.number}/locations/global/workloadIdentityPools/pi-cluster/providers/pi-cluster",
+        "//iam.googleapis.com/projects/${data.google_project.pi_cluster.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.pi_cluster.workload_identity_pool_id}/providers/pi-cluster",
         ]
     }
 }
 
 resource "google_service_account" "dns_solver" {
-    account_id   = "cert-manager-dns-solver"
+    account_id   = "cert-manager-dns-solver-sa"
     display_name = "Cert Manager DNS Solver"
 }
 
 resource "google_project_iam_member" "dns_solver_dns_admin" {
     project = data.google_project.pi_cluster.project_id
     role    = "roles/dns.admin"
-    member  = "serviceAccount:${google_service_account.dns_solver.email}" # cert-manager-dns-solver@pi-cluster-433101.iam.gserviceaccount.com
+    member  = "serviceAccount:${google_service_account.dns_solver.email}" # cert-manager-dns-solver-sa@pi-cluster-433101.iam.gserviceaccount.com
 }
 
-# k8s service account cert-manager to google service account cert-manager-dns-solver
+# k8s service account cert-manager to google service account cert-manager-dns-solver-sa
 resource "google_service_account_iam_member" "cert_manager_binding" {
     service_account_id = google_service_account.dns_solver.id
     role               = "roles/iam.workloadIdentityUser"
-    member             = "principal://iam.googleapis.com/projects/${data.google_project.pi_cluster.number}/locations/global/workloadIdentityPools/${local.K8S_WORKLOAD_IDENTITY_POOL}/subject/${local.K8S_NAMESPACE_CERT_MAN}/${local.K8S_SERVICE_ACCOUNT_CERT_MAN}"
+    member             = "principal://iam.googleapis.com/projects/${data.google_project.pi_cluster.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.pi_cluster.workload_identity_pool_id}/subject/${local.K8S_NAMESPACE_CERT_MAN}/${local.K8S_SERVICE_ACCOUNT_CERT_MAN}"
 }
