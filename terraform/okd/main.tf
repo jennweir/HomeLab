580547fdb70a6bd9de9edba2739cb471e3c31178
Jennifer Weir <contact@jenniferpweir.com>
2025-10-31 14:22:17 -0400

    fix(files): remove unrelated files

--- terraform/okd/main.tf
+++ terraform/okd/main.tf
@@ -52,9 +52,9 @@ resource "google_service_account" "gsm_accessor" {
     account_id   = "gsm-accessor"
     display_name = "GSM Accessor Service Account"
     project      = data.google_project.okd_homelab.project_id
-}	
+}
 
-resource "google_project_iam_member" "gsm_accessor_role" {	
+resource "google_project_iam_member" "gsm_accessor_role" {
     project = data.google_project.okd_homelab.project_id	
     role    = "roles/secretmanager.secretAccessor"
     member  = "serviceAccount:${google_service_account.gsm_accessor.email}"
