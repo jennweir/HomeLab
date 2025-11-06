1e76204ad3aec8cbbb801b7ee658b55921762e01
Jennifer Weir <91640709+jennweir@users.noreply.github.com>
2025-11-05 23:03:27 -0500

    feat(tests): workload identity federation (#113)
    * feat(tests): workload identity federation
    
    * feat(argo): authentication resource
    
    * feat(tf): test resources

--- terraform/okd/main.tf
+++ terraform/okd/main.tf
@@ -1,6 +1,8 @@
 locals {
     wif_pool = "okd-pool"
     wif_provider = "okd-provider"
+    smoke_tests_ns = "smoke-tests"
+    smoke_tests_sa = "smoke-tests-sa"
     cert_manager_ns = "cert-manager"
     openshift_monitoring_ns = "openshift-monitoring"
     eso_sa = "external-secrets"
@@ -9,6 +11,12 @@ locals {
 
 data "google_project" "okd_homelab" {
     project_id = "okd-homelab"
+}
+
+resource "google_project_service" "wif" {
+    project            = "${data.google_project.okd_homelab.project_id}"
+    service            = "sts.googleapis.com"
+    disable_on_destroy = false
 }
 
 resource "google_storage_bucket" "homelab_bucket" {
@@ -51,6 +59,20 @@ resource "google_iam_workload_identity_pool_provider" "okd_provider" {
     }
 }
 
+resource "google_service_account" "test_gsm_accessor" {
+    account_id   = "test-gsm-accessor"
+    display_name = "GSM Accessor Service Account for Testing"
+    project      = data.google_project.okd_homelab.project_id
+}
+
+resource "google_secret_manager_secret_iam_member" "test_secret_access" {
+    for_each  = toset(["test-secret"])
+    project   = data.google_project.okd_homelab.project_id
+    secret_id = each.value
+    role      = "roles/secretmanager.secretAccessor"
+    member    = "serviceAccount:${google_service_account.test_gsm_accessor.email}"
+}
+
 resource "google_service_account" "gsm_accessor" {
     account_id   = "gsm-accessor"
     display_name = "GSM Accessor Service Account"
@@ -65,13 +87,19 @@ resource "google_secret_manager_secret_iam_member" "secrets_access" {
     member    = "serviceAccount:${google_service_account.gsm_accessor.email}"
 }
 
-resource "google_service_account_iam_member" "cert_man_eso_wif" {
+resource "google_service_account_iam_member" "smoke_tests_wif_binding" {
+    service_account_id = "projects/${data.google_project.okd_homelab.project_id}/serviceAccounts/${google_service_account.test_gsm_accessor.email}"
+    role               = "roles/iam.workloadIdentityUser"
+    member             = "principal://iam.googleapis.com/projects/${data.google_project.okd_homelab.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.okd_pool.workload_identity_pool_id}/subject/system:serviceaccount:${local.smoke_tests_ns}:${local.smoke_tests_sa}"
+}
+
+resource "google_service_account_iam_member" "cert_man_eso_wif_binding" {
     service_account_id = "projects/${data.google_project.okd_homelab.project_id}/serviceAccounts/${google_service_account.gsm_accessor.email}"
     role               = "roles/iam.workloadIdentityUser"
     member             = "principal://iam.googleapis.com/projects/${data.google_project.okd_homelab.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.okd_pool.workload_identity_pool_id}/subject/system:serviceaccount:${local.cert_manager_ns}:${local.eso_sa}"
 }
 
-resource "google_service_account_iam_member" "openshift_monitoring_eso_wif" {
+resource "google_service_account_iam_member" "openshift_monitoring_eso_wif_binding" {
     service_account_id = "projects/${data.google_project.okd_homelab.project_id}/serviceAccounts/${google_service_account.gsm_accessor.email}"
     role               = "roles/iam.workloadIdentityUser"
     member             = "principal://iam.googleapis.com/projects/${data.google_project.okd_homelab.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.okd_pool.workload_identity_pool_id}/subject/system:serviceaccount:${local.openshift_monitoring_ns}:${local.eso_sa}"
