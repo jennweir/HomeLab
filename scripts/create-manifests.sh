c79d620351a55f9eec2f47fd9ab7ac1479d3d329
Jennifer Weir <91640709+jennweir@users.noreply.github.com>
2026-01-10 14:51:56 -0500

    chore(longhorn): update with v1.10.1 (#136)

--- scripts/create-manifests.sh
+++ scripts/create-manifests.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
-# CREDIT: https://github.com/dronenb/HomeLab/blob/main/kubernetes/workloads/cert-manager/create_manifests.sh
 set -o errexit
 set -o nounset
 set -o pipefail
 shopt -s failglob
 
+if [[ $# -ne 1 ]]; then
+  echo "Usage: $0 <manifest-url>"
+  exit 1
+fi
+
+MANIFESTURL="$1"
+
 mkdir -p base
 pushd base > /dev/null || exit 1
 
-MANIFESTURL="<CHANGE-ME>"
-
 # Download manifests and separate into separate files
 curl -sL "${MANIFESTURL}" | \
-    yq --no-colors --prettyPrint '... comments=""' | \
-    kubectl-slice -o . --template "{{ .kind | lower }}.yaml"
+  yq --no-colors --prettyPrint '... comments=""' | \
+  kubectl-slice -o . --template "{{ .kind | lower }}.yaml"
 
 # Split the deployment up
 kubectl-slice -o . --template "{{ .kind | lower }}-{{ .metadata.name | lower }}.yaml" < deployment.yaml
