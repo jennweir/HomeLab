8252fa67240850295508a455125a8ed71bde0682
Jennifer Weir <91640709+jennweir@users.noreply.github.com>
2026-05-09 13:20:22 -0400

    fix(argocd): azure secret locations (#153)
    * fix(argocd): azure secret locations
    
    * fix(argocd): azure secret locations

--- terraform/okd/variables.tf
+++ terraform/okd/variables.tf
@@ -1,8 +1,3 @@
-variable "homelab_project_id" {
-  description = "The GCP project ID for homelab management."
-  type        = string
-}
-
 variable "azure_tenant_id" {
   description = "The tenant ID for Azure Active Directory."
   type        = string
